f40d91869dcb7b1e95458faa987b34872d564dae
“isab554b” <isab554b@stud.kea.dk>
2022-02-03 21:18:43 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
+const { orange } = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      blaa: "#003060",
+      orange: "#E55B13",
+      beige: "#F5EDE0",
+      graa: "#CCCCCC",
+      hvid: "#FFF",
+    },
+
+    fontFamily: {
+      roboto: "'Roboto Condensed', sans-serif",
+    },
+
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
